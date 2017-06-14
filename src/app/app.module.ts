087db6ebc7fc77846d08e27682775c0bdd9598b1
Josh Cain <josh.cain@dealer.com>
2017-06-14 14:48:36 -0400

    Added basic files and folders for UI example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeedsComponent } from './feeds/feeds.component';
+import { FeedEditComponent } from './feeds/feed-edit/feed-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HeaderComponent, FeedsComponent, FeedEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/feeds/feeds.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Feed } from '../shared/feed.model';
+
+@Component({
+  selector: 'app-feeds',
+  templateUrl: './feeds.component.html',
+  styleUrls: ['./feeds.component.css']
+})
+export class FeedsComponent implements OnInit {
+  feeds: Feed[] = [
+    new Feed('vAuto', 5),
+    new Feed('DMI', 10),
+    new Feed('SomethingElse', 18)
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
