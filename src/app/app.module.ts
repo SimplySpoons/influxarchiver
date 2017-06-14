0d5c7f438e8b54c649513d156b9266d1e8739f68
Josh Cain <josh.cain@dealer.com>
2017-06-14 15:39:53 -0400

    updated to 4.2.2 in package json and changed some formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FeedsComponent } from './feeds/feeds.component';
 import { FeedEditComponent } from './feeds/feed-edit/feed-edit.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HeaderComponent, FeedsComponent, FeedEditComponent
+    HeaderComponent, FeedsComponent, FeedEditComponent, SidebarComponent
   ],
   imports: [
     BrowserModule
