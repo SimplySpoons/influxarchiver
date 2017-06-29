774b60c856f4500f36d6c0bf7fb504255dc1f448
ddchenryl <henry.latour@dealer.com>
2017-06-29 13:28:19 -0400

    fixed display issues

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  element: Element; 
+  sidebar_width: number; 
+  constructor(private elm: ElementRef) {
+    this.element = elm.nativeElement; 
+    console.log(this.element);
+
+   }
 
   ngOnInit() {
   }
