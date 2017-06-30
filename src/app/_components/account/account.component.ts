770102b120699061351f5cefc2c0939954806452
ddchenryl <henry.latour@dealer.com>
2017-06-29 20:13:51 -0400

    fixed loading issues

--- src/app/_components/account/account.component.ts
+++ src/app/_components/account/account.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { AccountService } from '../../_services/account.service';
+import { Account } from '../../_models/account'
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit {
 
-  constructor() { }
+
+export class AccountComponent implements OnInit {
+  searchTerm$ = new Subject<string>();
+  accounts: Array<Account> = [];
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
   }
+
+   searchForUser() {
+        /*this.accountService.search(this.searchTerm$).subscribe(name => {
+            this.accounts = this.accounts.concat(name);
+        });*/ 
+    }
 
 }
