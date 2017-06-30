5caef70c0bb62a36e67c2c559327a5efc3139e62
james cameron <james.cameron@dealer.com>
2017-06-29 21:53:42 -0400

    Adding Sidebar collapse logic - Adding logic to shrink main content with sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,12 @@ export class AppComponent {
     isFirstOpen: true,
     isFirstDisabled: false
   };
+
+  isIn = false;   // store state
+  toggleState() { // click handler
+    let bool = this.isIn;
+    this.isIn = bool === false ? true : false;
+    console.log("TEST " + bool)
+  }
+
 }
