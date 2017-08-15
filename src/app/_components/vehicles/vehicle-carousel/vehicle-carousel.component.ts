6f75955bb36a5f3300abf1e152d97ceffe9c7ec7
ddcjamesc <james.cameron@dealer.com>
2017-08-14 21:25:01 -0400

    made the img caROUsel scrolling work better

--- src/app/_components/vehicles/vehicle-carousel/vehicle-carousel.component.ts
+++ src/app/_components/vehicles/vehicle-carousel/vehicle-carousel.component.ts
@@ -10,17 +10,60 @@ export class VehicleCarouselComponent implements OnInit {
   @Input() vehicleImages: Array<any> = [];
   @Input() account: Account;
 
-  imgIndex = -1;
+  private _imgIndex: number;
+
+  get imgIndex(): number {
+    return this._imgIndex;
+  }
+
+  set imgIndex(newImgIndex: number) {
+    this._imgIndex = newImgIndex;
+    if (this.imgIndex) {
+       var wrap = this.elRef.nativeElement.querySelector('.carouselVehicleThumbnailsContainer');
+       var thumbWidth = wrap.querySelector('.vehicleThumbnail').offsetWidth;
+       var currentThumbLeft = this.imgIndex * thumbWidth;
+       var bumpFactor = 5;
+      //  console.log("imgIndex " + this.imgIndex + " currentThumbLeft: " + currentThumbLeft + " wrap width " + wrap.clientWidth + " thumb width " + thumbWidth + " bumpFactor " + bumpFactor + " wrap scroll left: " + wrap.scrollLeft)
+      //  console.log("if check: " + currentThumbLeft + " > " + (wrap.clientWidth - (thumbWidth * bumpFactor))  + wrap.scrollLeft );
+      //  console.log("else if check " + currentThumbLeft + " < " + thumbWidth + wrap.scrollLeft);
+       if (currentThumbLeft > ((wrap.clientWidth - (thumbWidth * bumpFactor)) + wrap.scrollLeft)) {
+
+           // We should scroll right a bit
+               wrap.scrollLeft = currentThumbLeft - wrap.clientWidth + (thumbWidth * bumpFactor);
+               console.log("laddy currentThumbLeft - wrap.clientWidth + (thumbWidth * bumpFactor) " + wrap.scrollLeft);
+       } else if (currentThumbLeft < (thumbWidth + wrap.scrollLeft)) {
+           // We should scroll left a bit
+               // Left position of thumb : scope.imgIndex * thumbWidth
+               wrap.scrollLeft = currentThumbLeft - thumbWidth;
+               console.log("lassy currentThumbLeft - thumbWidth " + wrap.scrollLeft);
+       }
+     }
+  }
 
   constructor(private elRef:ElementRef) { }
 
-  ngOnInit() {
-
-  }
-
+  ngOnInit() { }
 
   ngDoCheck() {
+    //
+      // if (this.imgIndex !== 0) {
+      //   let wrap = element.all(by.tagName('.thumb-wrap').get(0));
+      //
+      //
+      //
+      // }
+   }
+
+  getImageSrc(data: any, accountId: string) {
+    let firstLetter = accountId.charAt(0);
+    let url = 'https://pictures.dealer.com/ddc/resize/320x/quality/70/sharpen/1/ddc/' + firstLetter + '/' + accountId + '/' + data;
+    return url;
+  }
+
+  test() {
+    alert();
     if (this.imgIndex >= 0) {
+      alert("wee");
       var wrap = this.elRef.nativeElement.querySelector('.carouselVehicleThumbnailsContainer');
       var thumbWidth = wrap.querySelector('.vehicleThumbnail').offsetWidth;
       var currentThumbLeft = this.imgIndex * thumbWidth;
@@ -40,21 +83,5 @@ export class VehicleCarouselComponent implements OnInit {
               console.log("lassy currentThumbLeft - thumbWidth " + wrap.scrollLeft);
       }
     }
-      // if (this.imgIndex !== 0) {
-      //   let wrap = element.all(by.tagName('.thumb-wrap').get(0));
-      //
-      //
-      //
-      // }
-   }
-
-  getImageSrc(data: any, accountId: string) {
-    let firstLetter = accountId.charAt(0);
-    let url = 'https://pictures.dealer.com/ddc/resize/320x/quality/70/sharpen/1/ddc/' + firstLetter + '/' + accountId + '/' + data;
-    return url;
-  }
-
-  test() {
-    alert("something" + this.imgIndex);
   }
 }
