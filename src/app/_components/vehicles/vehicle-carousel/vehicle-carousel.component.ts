128564820a8f99229dfd124638c29200c839e218
ddcjamesc <james.cameron@dealer.com>
2017-08-14 22:45:09 -0400

    caROUsel is sexy now

--- src/app/_components/vehicles/vehicle-carousel/vehicle-carousel.component.ts
+++ src/app/_components/vehicles/vehicle-carousel/vehicle-carousel.component.ts
@@ -18,14 +18,14 @@ export class VehicleCarouselComponent implements OnInit {
 
   set imgIndex(newImgIndex: number) {
     this._imgIndex = newImgIndex;
-    if (this.imgIndex) {
+    if (this.imgIndex >= 0) {
        var wrap = this.elRef.nativeElement.querySelector('.carouselVehicleThumbnailsContainer');
        var thumbWidth = wrap.querySelector('.vehicleThumbnail').offsetWidth;
        var currentThumbLeft = this.imgIndex * thumbWidth;
-       var bumpFactor = 5;
-      //  console.log("imgIndex " + this.imgIndex + " currentThumbLeft: " + currentThumbLeft + " wrap width " + wrap.clientWidth + " thumb width " + thumbWidth + " bumpFactor " + bumpFactor + " wrap scroll left: " + wrap.scrollLeft)
-      //  console.log("if check: " + currentThumbLeft + " > " + (wrap.clientWidth - (thumbWidth * bumpFactor))  + wrap.scrollLeft );
-      //  console.log("else if check " + currentThumbLeft + " < " + thumbWidth + wrap.scrollLeft);
+       var bumpFactor = 2;
+       console.log("imgIndex " + this.imgIndex + " currentThumbLeft: " + currentThumbLeft + " wrap width " + wrap.clientWidth + " thumb width " + thumbWidth + " bumpFactor " + bumpFactor + " wrap scroll left: " + wrap.scrollLeft)
+       console.log("if check: " + currentThumbLeft + " > " + (wrap.clientWidth - (thumbWidth * bumpFactor))  + wrap.scrollLeft );
+       console.log("else if check " + currentThumbLeft + " < " + thumbWidth + wrap.scrollLeft);
        if (currentThumbLeft > ((wrap.clientWidth - (thumbWidth * bumpFactor)) + wrap.scrollLeft)) {
 
            // We should scroll right a bit
@@ -44,44 +44,18 @@ export class VehicleCarouselComponent implements OnInit {
 
   ngOnInit() { }
 
-  ngDoCheck() {
-    //
-      // if (this.imgIndex !== 0) {
-      //   let wrap = element.all(by.tagName('.thumb-wrap').get(0));
-      //
-      //
-      //
-      // }
-   }
-
   getImageSrc(data: any, accountId: string) {
     let firstLetter = accountId.charAt(0);
-    let url = 'https://pictures.dealer.com/ddc/resize/320x/quality/70/sharpen/1/ddc/' + firstLetter + '/' + accountId + '/' + data;
+    let url = 'https://pictures.dealer.com//' + firstLetter + '/' + accountId + '/' + data;
     return url;
   }
 
-  test() {
-    alert();
-    if (this.imgIndex >= 0) {
-      alert("wee");
-      var wrap = this.elRef.nativeElement.querySelector('.carouselVehicleThumbnailsContainer');
-      var thumbWidth = wrap.querySelector('.vehicleThumbnail').offsetWidth;
-      var currentThumbLeft = this.imgIndex * thumbWidth;
-      var bumpFactor = 5;
-      console.log("imgIndex " + this.imgIndex + " currentThumbLeft: " + currentThumbLeft + " wrap width " + wrap.clientWidth + " thumb width " + thumbWidth + " bumpFactor " + bumpFactor + " wrap scroll left: " + wrap.scrollLeft)
-      console.log("if check: " + currentThumbLeft + " > " + (wrap.clientWidth - (thumbWidth * bumpFactor))  + wrap.scrollLeft );
-      console.log("else if check " + currentThumbLeft + " < " + thumbWidth + wrap.scrollLeft);
-      if (currentThumbLeft > ((wrap.clientWidth - (thumbWidth * bumpFactor)) + wrap.scrollLeft)) {
-
-          // We should scroll right a bit
-              wrap.scrollLeft = currentThumbLeft - wrap.clientWidth + (thumbWidth * bumpFactor);
-              console.log("laddy currentThumbLeft - wrap.clientWidth + (thumbWidth * bumpFactor) " + wrap.scrollLeft);
-      } else if (currentThumbLeft < (thumbWidth + wrap.scrollLeft)) {
-          // We should scroll left a bit
-              // Left position of thumb : scope.imgIndex * thumbWidth
-              wrap.scrollLeft = currentThumbLeft - thumbWidth;
-              console.log("lassy currentThumbLeft - thumbWidth " + wrap.scrollLeft);
-      }
-    }
+  getImageThumbSrc(data: any, accountId: string) {
+    let firstLetter = accountId.charAt(0);
+    let url = 'https://pictures.dealer.com//' + firstLetter + '/' + accountId + '/' + data;
+    var fileName = url.substring(url.lastIndexOf('/') + 1);
+    return url.replace(fileName, 'thumb_' + fileName);
   }
+
+
 }
