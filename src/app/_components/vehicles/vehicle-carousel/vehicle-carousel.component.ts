0d3a35a2b083ce4a3a40541fee7610ccaa0c332b
ddcjamesc <james.cameron@dealer.com>
2017-08-18 14:45:24 -0400

    your mother

--- src/app/_components/vehicles/vehicle-carousel/vehicle-carousel.component.ts
+++ src/app/_components/vehicles/vehicle-carousel/vehicle-carousel.component.ts
@@ -18,27 +18,23 @@ export class VehicleCarouselComponent implements OnInit {
 
   set imgIndex(newImgIndex: number) {
     this._imgIndex = newImgIndex;
-    console.log("IMG INDEX: " + this._imgIndex);
-    if (this._imgIndex >= 0) {
-      console.log("IN HERE " + this._imgIndex);
-       var wrap = this.elRef.nativeElement.querySelector('.carouselVehicleThumbnailsContainer');
-       var thumbWidth = wrap.querySelector('.vehicleThumbnail').offsetWidth;
-       var currentThumbLeft = this._imgIndex * thumbWidth;
-       var bumpFactor = 5;
-      //  console.log("imgIndex " + this.imgIndex + " currentThumbLeft: " + currentThumbLeft + " wrap width " + wrap.clientWidth + " thumb width " + thumbWidth + " bumpFactor " + bumpFactor + " wrap scroll left: " + wrap.scrollLeft)
-      //  console.log("if check: " + currentThumbLeft + " > " + (wrap.clientWidth - (thumbWidth * bumpFactor))  + wrap.scrollLeft );
-      //  console.log("else if check " + currentThumbLeft + " < " + thumbWidth + wrap.scrollLeft);
-      if (currentThumbLeft > ((wrap.clientWidth - (thumbWidth * bumpFactor)) + wrap.scrollLeft)) {
 
+    var wrap = this.elRef.nativeElement.querySelector('.carouselVehicleThumbnailsContainer');
+    if (this.imgIndex !== 0) {
+       var thumbWidth = wrap.querySelector('.vehicleThumbnail').offsetWidth;
+       var currentThumbLeft = this.imgIndex * thumbWidth;
+       var bumpFactor = 5;
+
+       if (currentThumbLeft > ((wrap.clientWidth - (thumbWidth * bumpFactor)) + wrap.scrollLeft)) {
            // We should scroll right a bit
-               wrap.scrollLeft = currentThumbLeft - wrap.clientWidth + (thumbWidth * bumpFactor);
-               console.log("laddy currentThumbLeft - wrap.clientWidth + (thumbWidth * bumpFactor) " + wrap.scrollLeft);
+           wrap.scrollLeft = currentThumbLeft - wrap.clientWidth + (thumbWidth * bumpFactor);
        } else if (currentThumbLeft < (thumbWidth + wrap.scrollLeft)) {
            // We should scroll left a bit
-               // Left position of thumb : scope.imgIndex * thumbWidth
-               wrap.scrollLeft = currentThumbLeft - thumbWidth;
-               console.log("lassy currentThumbLeft - thumbWidth " + wrap.scrollLeft);
+           // Left position of thumb : scope.imgIndex * thumbWidth
+           wrap.scrollLeft = currentThumbLeft - thumbWidth;
        }
+     } else {
+       wrap.scrollLeft = 0;
      }
   }
 
@@ -46,6 +42,9 @@ export class VehicleCarouselComponent implements OnInit {
 
   ngOnInit() { }
 
+  ngOnChanges() {
+    this.imgIndex = 0;
+  }
   getImageSrc(data: any, accountId: string) {
     console.log("get image " + accountId + " data " + data);
     let firstLetter = accountId.charAt(0);
