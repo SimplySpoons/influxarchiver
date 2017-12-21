11f741fec02135026ddc70eafb4d2591e8b46c71
Josh Cain <josh.cain@dealer.com>
2017-12-21 13:14:36 -0500

    fixed api stuff in user service

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -5,32 +5,34 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
+import { AppConfig } from '../app.config';
 
 @Injectable()
 export class UserService {
-    
-    constructor(private http: Http) {
-        // this.single = new Average();
+    API_URL;
+
+    constructor(private http: Http, private conf: AppConfig) {
+        this.API_URL = this.conf.API_CONFIG();
     }
 
     // NONE OF THESE PHP FILES EXIST YET, WE NEED TO CREATE A DATABASE TO HOUSE ALL THE BUGS, AND CREATE ALL OF THESE FILES. BEFORE NOW WE WERE JUST USING THE TICKETRATER DATABASE BUT WE NEED A SEPARATE ONE FOR INFLUXARCHIVER2
     getAllBugs(status: number) {
-        return this.http.post('https://supporttoolbox.dealerdotcom.corp/var/www/html/staging/hotdog/php/bugfunctions.php', { request: "getAllBugs", status: status }).map(
+        return this.http.post(this.API_URL + 'bugfunctions.php', { request: "getAllBugs", status: status }).map(
             (response: Response) => response.json());
     }
 
     addBugNow(newBug: any) {
-        return this.http.post('https://supporttoolbox.dealerdotcom.corp/var/www/html/staging/hotdog/php/bugfunctions.php', { request: "addBug", newBug: newBug }).map(
+        return this.http.post(this.API_URL + 'bugfunctions.php', { request: "addBug", newBug: newBug }).map(
             (response: Response) => response.json());
     }
 
     markBugComplete(id: string) {
-        return this.http.post('https://supporttoolbox.dealerdotcom.corp/var/www/html/staging/hotdog/php/bugfunctions.php', { request: "markBugComplete", id: id }).map(
+        return this.http.post(this.API_URL + 'bugfunctions.php', { request: "markBugComplete", id: id }).map(
             (response: Response) => response.json());
     }
 
     deleteBug(id: string) {
-        return this.http.post('https://supporttoolbox.dealerdotcom.corp/var/www/html/staging/hotdog/php/bugfunctions.php', { request: "deleteBug", id: id }).map(
+        return this.http.post(this.API_URL + 'bugfunctions.php', { request: "deleteBug", id: id }).map(
             (response: Response) => response.json());
     }
 }
