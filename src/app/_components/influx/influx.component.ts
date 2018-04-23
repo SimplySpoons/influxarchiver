df662149c69e1264063e0ced80cfb46de8bad569
Luke Rossi <luke.rossi@dealer.com>
2018-04-23 15:20:52 -0400

    Implemented individual feed tab functionality on feed view page

--- src/app/_components/influx/influx.component.ts
+++ src/app/_components/influx/influx.component.ts
@@ -41,7 +41,7 @@ export class InfluxComponent implements OnInit, OnDestroy {
   count_sub: any;
   counts: any;
   params: Provider = new Provider();
-
+  showIndex = -1;
 
   constructor(private accountService: AccountService,
     private route: ActivatedRoute, private router: Router,
@@ -57,7 +57,9 @@ export class InfluxComponent implements OnInit, OnDestroy {
       this.influx.setFileList(config.files.data);
     }
   }
-
+  setConfigToShow(index) {
+    this.showIndex = index;
+  }
   toggleFilter(config) {
     if (this.filters[config.provider] && !config.filters) {
       config.filters = this.filters[config.provider];
